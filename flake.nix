40f602d206b48591aa258e0b6dad74ea76f3ab0f
Nathan Henrie <nate@n8henrie.com>
2026-06-04 12:46:01 -0600

    Use (deprecated) ssl from nixpkgs 21.11

--- flake.nix
+++ flake.nix
@@ -2,16 +2,29 @@
   description = "Flake to resurrect Notational Velocity";
 
   # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
 
   outputs =
     { self, nixpkgs, ... }:
     let
       system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.permittedInsecurePackages = [ "openssl-1.0.2u" ];
+      };
     in
     {
-      packages.${system}.default = pkgs.callPackage ./package.nix { };
+      packages.${system} = {
+        default = pkgs.callPackage ./package.nix { };
+        deps = pkgs.symlinkJoin {
+          name = "nvalt-deps";
+          paths = with pkgs.openssl_1_0_2; [
+            out
+            dev
+          ];
+        };
+      };
       devShells.${system}.default = pkgs.mkShell {
         inputsFrom = [ self.outputs.packages.${system}.default ];
       };
